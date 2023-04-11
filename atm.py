3b8b430b0916f89d1ace41affc2c2ed8087948a0
delhiganesh <ramanavijayakumar73061@gmail.com>
2023-04-11 19:19:04 +0530

    upgrade

--- atm.py
+++ atm.py
@@ -1,12 +1,13 @@
 import random
 
 
-class atm:
+class Atm:
 
-    def __init__(self, username, password):
+    def __init__(self, username, account_no, password, balance=0):
         self.username = username
         self.password = password
-        self.remaining_balance = 1000
+        self.accountNo = account_no
+        self.remaining_balance = balance
 
     def otp(self):
         otp = random.randint(100000, 999999)
@@ -17,51 +18,74 @@ class atm:
         else:
             return False
 
+    def info(self):
+        if self.otp():
+            print(
+                f'\tUsername : {self.username}\n\tAccount Number :{self.accountNo}\n\tBalance :{self.remaining_balance}\n\t', "-"*10)
+        else:
+            print("Invalid otp!!!Try Again!!!\n\t", "-"*10)
+
     def balance(self):
         if self.otp():
-            print(f"Your Balance is : Rs {self.remaining_balance}")
+            print(f"Your Balance is : Rs {self.remaining_balance}\n\t", "-"*10)
         else:
-            print("Invalide otp")
+            print("Invalid otp!!!Try Again!!!\n\t", "-"*10)
 
     def deposit(self):
         amount = int(
             input("Enter the amount to be deposited to your account: "))
         if self.otp():
             self.remaining_balance += amount
-            print(f"Current Balance:{self.remaining_balance}")
+            print(f"Current Balance:{self.remaining_balance}\n\t", "-"*10)
         else:
-            print("Invalid otp")
+            print("Invalid otp!!!Try Again!!!\n\t", "-"*10)
 
     def withdrawal(self):
         amount = int(
             input("Enter the amount to be withdrawn from your account: "))
         if self.otp():
-            self.remaining_balance -= amount
-            print(f"Remaining Balance:{self.remaining_balance}")
+            if amount < self.remaining_balance:
+                self.remaining_balance -= amount
+                print(
+                    f"Withdrawn successfully!!!\nRemaining Balance:{self.remaining_balance}\n\t", "-"*10)
+            else:
+                print("Your balance is insufficient!!!Try Again!!!\n\t", "-"*10)
         else:
-            print("Invalid otp")
+            print("Invalid otp!!!Try Again!!!\n\t", "-"*10)
 
     def operation(self):
-        while (1):
-            print("\t1:Balance Enquiry\n\t2:Deposit\n\t3:Withdrawal\n\t4:Exit")
+        loop = True
+        while loop:
+            print(
+                "\t1:Account Details\n\t2.Balance Enquiry\n\t3:Deposit\n\t4:Withdrawal\n\t5:Exit")
             option = int(input("Select the Operations you want:"))
             if option == 1:
-                self.balance()
-                continue
+                self.info()
+
             elif option == 2:
-                self.deposit()
-                continue
+                self.balance()
+
             elif option == 3:
-                self.withdrawal()
-                continue
+                self.deposit()
             elif option == 4:
+                self.withdrawal()
+            elif option == 5:
                 print("Thank you for using our service!!!")
-                break
+                print("-"*40)
+                loop = False
             else:
-                print("Invalid option!!!Please Enter a valid option!!!")
+                print("Invalid option!!!Please Enter a valid option!!!\n\t")
 
 
-username = input("Enter the Username:")
-password = input("Enter your password")
-user1 = atm(username, password)
-user1.operation()
+user = Atm("Delhi Ganesh V".upper(), 887079286, "kumar123", 1000)
+
+while True:
+    print("-"*40)
+    username = input("Enter the Username:").upper()
+    password = input("Enter your password")
+    if username == user.username and password == user.password:
+        user.operation()
+    else:
+        print("Username and password are incorrect!!!Try again!!!")
+
+        continue
