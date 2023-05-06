e4551fee63511defb89256312d7242a7a8e0c6ab
delhiganesh <ramanavijayakumar73061@gmail.com>
2023-05-06 08:12:24 +0530

    update sound

--- Assignment_2.py
+++ Assignment_2.py
@@ -1,24 +1,33 @@
-import random
+import random    # for generating values
 import time
-from playsound import playsound
+import winsound  # for buzzer sound
+
+'''Define the threshold level for temperature and humidity'''
 TEM_THRESHOLD = 30
-HUM_THRESHOLD = 80
+HUM_THRESHOLD = 70
+
+'''Define a function for generation of temperature and humidity values'''
 
 
 def value_gen():
-    temperature = random.uniform(20, 50)
-    humidity = random.uniform(40, 100)
+    # generate temperature between 20 to 50 degree celsius
+    temperature = random.uniform(20, 60)
+    # generate humidity between 40% to 90%
+    humidity = random.uniform(40, 90)
     return temperature, humidity
 
 
+# Loop to generate values and check for alarms
 while True:
     temp, hum = value_gen()
-    print(temp, hum, "\n")
+    print(f'Temperature : {temp:.2f} || Humidity : {hum:.2f}')
+    '''Check for temperature and humidity alarms'''
     if temp > TEM_THRESHOLD:
-        print("Temperature Alarm Triggerred")
-        playsound("alarm.wav")
+        print("Temperature Alarm Triggered!!!")
+        winsound.Beep(3150, 2000)  # generate buzzer sound for 2 seconds
     if hum > HUM_THRESHOLD:
-        print("Humidity alarm triggerred")
-        playsound("alarm.wav")
-
-#    time.sleep(1)
+        print("Humidity Alarm Triggered!!!")
+        winsound.Beep(3150, 2000)  # generate buzzer sound for 2seconds
+    print("\n")
+    # wait for some time before generating the value
+    time.sleep(2)
